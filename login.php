8e78caf1486d5543739bebcfbc8c4f926b6d02eb
NithiMalan <56369839+NithiMalan@users.noreply.github.com>
2020-07-29 18:24:07 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,54 @@
+<?php
+session_start();
+session_unset();
+session_destroy();
+?>
+
+<html>
+<head>
+<title>Login Page</title>
+<link href="style.css" rel="stylesheet" type="text/css" />
+<style>
+body{
+	
+	background-image:url("diva2.jpg") ;
+	background-repeat: no-repeat ;
+	background-attachment: fixed;
+    
+	background-size: cover;
+	
+}
+h2 { color:#900C3F;}
+h3 { color:#F1C40F;}</style>
+</head>
+<body>
+<div id="main">
+  <!-- content -->
+  <div id="content">
+    <div class="indent1">
+      <h1>Login</h1>
+      <h3>Please enter your login information.</h3>
+      <form name="loginForm" method="post" action="https://reqres.in/api/login">
+      <table align="center">
+        <tr>
+          <td style="color:#89b700"><b>Login</b></td>
+          <td><input name="login" type="text" id="login" /></td>
+        </tr>
+		<tr>
+          <td style="color:#89b700"><b>Password</b></td>
+          <td><input name="password" type="password"/></td>
+        </tr>
+		<tr>
+		  <td>	</td>
+		  <td>	</td>
+		 </tr>
+		<tr>
+          <td colspan="2" align="center" >
+            <input type="submit" name="submit" value="Login" /></td>
+         </tr>
+      </table>
+    </div>
+  </div>
+</div>
+</body>
+</html>
